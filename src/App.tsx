796057b2d27a1bba5eac258281714030bd19288a
tupy77 <100968748+tupy77@users.noreply.github.com>
2024-12-05 12:38:23 +0100

    Update App.tsx
    Prueba para el deploy

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Esto es para prueba de deploy</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
